71c3fea0b4f62c639904755aea1746c5e4dff772
MohammedMaisoon <mmohammedmaisoon@gmail.com>
2024-04-24 23:51:11 +0530

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+    public static void main(String[] args) {
+        new Animal("lion",3);
+    }
+    public Animal(){
+        System.out.println("Empty Constructor");
+
+    }
+    public Animal(String a){
+        System.out.println("Constructor With String Parameter");
+    }
+    public Animal(String a,int b){
+        System.out.println("Constructor With String and int Parameter");
+    }
+
+}
+
+
+
+
